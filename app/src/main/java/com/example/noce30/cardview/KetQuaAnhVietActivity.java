b242093fa1cb86027611d66dbb69fd01d220249a
vanhoang1001 <lvanhoang1998@gmail.com>
2019-04-07 16:37:40 +0700

    tudiendidong2
    update mac dinh ban dau

---
+++ app/src/main/java/com/example/noce30/cardview/KetQuaAnhVietActivity.java
@@ -0,0 +1,19 @@
+package com.example.noce30.cardview;
+
+//Lê Văn Hoàng - cập nhật 14:05 07/04/2019
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class KetQuaAnhVietActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_tratiengviet);
+    }
+}
